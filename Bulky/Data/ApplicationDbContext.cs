4f305284e25cf40f3ea268565174f8067a830f48
Artur <artur.talimonchyk@gmail.com>
2024-04-02 11:12:59 +0200

    AddedDbContext_SeededCategories

---
+++ Bulky/Data/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Bulky.Models;
+namespace Bulky.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        DbSet<Category> Categories { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category
+                {
+                    Id =1,
+                    Name = "Machines",
+                    DisplayOrder = 1
+                },
+                 new Category
+                 {
+                     Id = 2,
+                     Name = "Laundries",
+                     DisplayOrder = 2
+                 },
+                  new Category
+                  {
+                      Id = 3,
+                      Name = "PC",
+                      DisplayOrder = 3
+                  }
+                );
+        }
+    }
+}
