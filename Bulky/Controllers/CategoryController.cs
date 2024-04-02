ad3c6af873b41f9bb4ae8c343c65dbfb9291a97d
Artur <artur.talimonchyk@gmail.com>
2024-04-02 11:29:07 +0200

    Added CategoryController

--- Bulky/Controllers/CategoryController.cs
+++ Bulky/Controllers/CategoryController.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bulky.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bulky.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
--- Bulky/Program.cs
+++ Bulky/Program.cs
@@ -33,6 +33,9 @@ class Program
             name: "default",
             pattern: "{controller=Home}/{action=Index}/{id?}");
 
+        
+
+
         app.Run();
     }
 }
